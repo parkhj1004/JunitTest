563c7c01797b0a271ca02a072966f8241bbb4a34
HYOJIN PARK <hyojinpark.world@gmail.com>
2019-07-30 16:56:09 +0900

    .

--- src/DoitEx.java
+++ src/DoitEx.java
@@ -5,6 +5,34 @@ public class DoitEx {
     @Test
     public void ex2_10() {
 
+        int count = 0;
+        int ptr = 0;
+        int[] prime = new int[500];
+
+        prime[ptr++] = 2;
+
+        for(int n = 3; n <= 1000 ; n += 2) {
+            int i;
+            for(i = 1; i < ptr; i++) {
+                count++;
+
+                if (n % prime[i] == 0) {
+                    break;
+                }
+            }
+
+            if(ptr == i) {
+                prime[ptr++] = n;
+            }
+        }
+
+        for(int p : prime) {
+            System.out.println(p);
+        }
+
+        System.out.printf("나눗셈을 수행한 횟수 : %d\n" , count);
+
+
     }
 
     @Test
